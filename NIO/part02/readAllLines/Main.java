c16227b3f6f9bc69fff9acff06afebee952fad6c
Gorhaf <gorhaf@163.com>
2022-03-01 11:05:32 +0800

    02：一行代码搞定读数据
    看动画，学Java教程NIO篇：一行代码搞定读数据

---
+++ NIO/part02/readAllLines/Main.java
@@ -0,0 +1,48 @@
+package main;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * @author 【看动画，学Java】https://learning.snssdk.com/feoffline/toutiao_wallet_bundles/toutiao_learning_wap/online/album_detail.html?content_id=6892772675148579083
+ * @author 【官方网站】gorhaf.com
+ * @author 【微信公众号】gorhaf
+ * @author 【个人微信号】gorhafapp
+ * @author 【抖音/今日头条/西瓜视频】人人都是程序员
+ * @author 【腾讯课堂】人人都是程序员
+ * @author 【B站】人人都是程序员
+ * 欢迎大家扫描下方二维码关注我们
+ * █▀▀▀▀▀▀▀██▀██████▀▀▀▀██▀▀▀▀▀▀▀█
+ * █ █▀▀▀█ █▄ ▀ ▄▄█▄█▄█▀██ █▀▀▀█ █
+ * █ █   █ █▄▄▀▄▀█▄▄ ██▀ █ █   █ █
+ * █ ▀▀▀▀▀ █ █ █▀▄▀▄ █ █ █ ▀▀▀▀▀ █
+ * █▀▀▀▀▀█▀▀▀▀▀█ █▀▄▀▀█ ▄▀█▀█▀█▀██
+ * █▄▄▄ ██▀█  █▀▀ ▀  ▄ ▄▀▄   ███▄█
+ * ██  ▀▀█▀ ▄ ▄ █▀▄▄▄▀▄▀▄ ▄██▀▀▄██
+ * █ █▄▄  ▀ █ ▄█▄▄▄▀▄▀█▄ ▄ ▄ █ █▄█
+ * █  ▄██▄▀██▄ █ ▀▄▀ ▀▀ ▄▄▄▀██▀▄██
+ * █ █▀▀█▀▀█▀ █▀▀█▄▀▀█▀▄ ▀▄    █▄█
+ * █ █▀▀██▀▀█▀  █▀▀  ██▄▀▀▀▀▀█▀ ▀█
+ * █▀▀▀▀▀▀▀█ ▄██▄▄▀▀▀▄▄▀ █▀█   ▄▄█
+ * █ █▀▀▀█ █▀ ▄█ █▀ ▄▀▄▄ ▀▀▀  ▀▀ █
+ * █ █   █ █ ▄▀▀▀█▄ ▄▀▄▀ █▀▀▀ ▀ ▄█
+ * █ ▀▀▀▀▀ █ ▀█ ▀█▄  █▄█▄▄█  ▄▀▄██
+ * ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            // 读取数据
+            List<String> lines = Files.readAllLines(Paths.get("/Users/admin/Downloads/人人都是程序员.txt"));
+            // 输出数据
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
