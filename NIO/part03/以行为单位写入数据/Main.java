d632833ac3d54e7dc68750cb4dd3381883c2b9f6
Gorhaf <gorhaf@163.com>
2022-03-01 11:09:55 +0800

    03：一行代码搞定写数据
    看动画，学Java教程NIO篇：一行代码搞定写数据

---
+++ NIO/part03/以行为单位写入数据/Main.java
@@ -0,0 +1,54 @@
+package main;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 【看动画，学Java】https://learning.snssdk.com/feoffline/toutiao_wallet_bundles/toutiao_learning_wap/online/album_detail.html?content_id=6892772675148579083
+ * @author 【官方网站】gorhaf.com
+ * @author 【微信公众号】gorhaf
+ * @author 【个人微信号】gorhafapp
+ * @author 【抖音/今日头条/西瓜视频】人人都是程序员
+ * @author 【腾讯课堂】人人都是程序员
+ * @author 【B站】人人都是程序员
+ * 欢迎大家扫描下方二维码关注我们
+ * █▀▀▀▀▀▀▀██▀██████▀▀▀▀██▀▀▀▀▀▀▀█
+ * █ █▀▀▀█ █▄ ▀ ▄▄█▄█▄█▀██ █▀▀▀█ █
+ * █ █   █ █▄▄▀▄▀█▄▄ ██▀ █ █   █ █
+ * █ ▀▀▀▀▀ █ █ █▀▄▀▄ █ █ █ ▀▀▀▀▀ █
+ * █▀▀▀▀▀█▀▀▀▀▀█ █▀▄▀▀█ ▄▀█▀█▀█▀██
+ * █▄▄▄ ██▀█  █▀▀ ▀  ▄ ▄▀▄   ███▄█
+ * ██  ▀▀█▀ ▄ ▄ █▀▄▄▄▀▄▀▄ ▄██▀▀▄██
+ * █ █▄▄  ▀ █ ▄█▄▄▄▀▄▀█▄ ▄ ▄ █ █▄█
+ * █  ▄██▄▀██▄ █ ▀▄▀ ▀▀ ▄▄▄▀██▀▄██
+ * █ █▀▀█▀▀█▀ █▀▀█▄▀▀█▀▄ ▀▄    █▄█
+ * █ █▀▀██▀▀█▀  █▀▀  ██▄▀▀▀▀▀█▀ ▀█
+ * █▀▀▀▀▀▀▀█ ▄██▄▄▀▀▀▄▄▀ █▀█   ▄▄█
+ * █ █▀▀▀█ █▀ ▄█ █▀ ▄▀▄▄ ▀▀▀  ▀▀ █
+ * █ █   █ █ ▄▀▀▀█▄ ▄▀▄▀ █▀▀▀ ▀ ▄█
+ * █ ▀▀▀▀▀ █ ▀█ ▀█▄  █▄█▄▄█  ▄▀▄██
+ * ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // 要输出的数据
+        List<String> lines = new ArrayList<>();
+        lines.add("看动画");
+        lines.add("学编程");
+        lines.add("就请关注「人人都是程序员」");
+        // 写入的文件的路径
+        Path path = Paths.get("/Users/admin/Downloads/人人都是程序员.txt");
+        try {
+            // 写入数据
+            Files.write(path, lines, StandardOpenOption.CREATE);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
